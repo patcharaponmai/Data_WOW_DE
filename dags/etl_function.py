4af6c0eb03d98c52494c24e54e584045aa560bcb
Patcharapon <140698887+patcharaponmai@users.noreply.github.com>
2023-09-17 16:35:58 +0700

    Add files via upload

--- dags/etl_function.py
+++ dags/etl_function.py
@@ -2,12 +2,25 @@ import os
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
-from datetime import datetime
+import time
+from datetime import datetime, timedelta
 from airflow.hooks.postgres_hook import PostgresHook
+from multiprocessing import Pool
+
+# Create decorator for compute runtime
+def runtime(func):
+  def wrapper(*args, **kwargs):
+    start_time = time.time()
+    result = func(*args, **kwargs)
+    end_time = time.time()
+    runtime = end_time - start_time
+    print(f"{func.__name__} took {runtime} seconds to execute.")
+    return result
+  return wrapper
 
 class ETL:
 
-  def __init__(self, source_file_path : str, connection : str, tgt_table : str, BatchSize : int = 2e6) -> None:
+  def __init__(self, source_file_path : str, connection : str, tgt_table : str) -> None:
 
     """
     Initialize the ETL object.
@@ -16,21 +29,18 @@ class ETL:
         - source_file_path (str): The path to the source file location.
         - connection (str): The Airflow connection ID for the PostgreSQL database.
         - tgt_table (str): The name of the target table for data ingestion.
-        - BatchSize (int, optional): The size of batches for data insertion (default=2e6).
     """
 
     self.df = pd.DataFrame()
-    self.USER = os.getenv("POSTGRES_USER")
-    self.PASSWORD = os.getenv("POSTGRES_PASSWORD")
-    self.DB = os.getenv("POSTGRES_DB")
     self.source_file_path = source_file_path
+    self.output_csv_path = f'{self.source_file_path}/output_csv' # Collect output csv file
     self.connection = connection
     self.tgt_table = tgt_table
-    self.BatchSize = BatchSize
+    self.column_names = [] # Collect list of table's column name
+    self.list_total_rec = [] # Collect list of total record in each file
 
   # Create a function for connecting to the PostgreSQL database.
-
-  def create_connection_postgresql(self):
+  def create_connection_postgresql(self) -> None:
     try:
       pg_hook = PostgresHook(postgres_conn_id=self.connection)
       conn = pg_hook.get_conn()
@@ -39,179 +49,248 @@ class ETL:
     except Exception as e:
       print("Error connecting to PostgreSQL:", e)
       return None
-  
-  # Create a function to execute various queries, mainly to catch errors that may occur when running queries related to DELETE, UPDATE, INSERT to prevent data loss.
 
-  def execute_query(self, conn, cur, QUERY : str, VALUES : tuple = None):
-
-    """
-    Execute query.
-
-    Parameters:
-        - conn: A PostgreSQL database connection.
-        - cur: A database cursor.
-        - QUERY (str): The SQL query statement to execute.
-        - VALUES (tuple, optional): Optional tuple of values to insert into placeholders in the query (default=None).
-    """
-
-    try:
-      cur.execute(QUERY, VALUES)
-      conn.commit()
-    except Exception as e:
-      conn.rollback()
-      print(f"Execute query Failed", e)
-
+  @runtime
   # Create a function for extract data.
   def extract_data(self):
 
     print("########################")
     print("##### EXTRACT DATA #####")
     print("########################")
+
     print("Start extracting data ...")
+    print("Listing file in source file path ...")
+
     list_file = os.listdir(self.source_file_path)
 
     # List only parquet file in folder to aviod other file in case that folder contain various extension file
+    print("List parquet file ...")
     parquet_files = [file for file in list_file if file.endswith('.parquet')]
 
-    # Combine Parquet files into a single dataset
-    start_time = datetime.now()
-
     # Apply PyArrow library for handling large datasets using Arrow memory structures. 
+    print("Read parquet file as table ...")
     tables = [pq.read_table(os.path.join(self.source_file_path, file)) for file in parquet_files]
 
+    # Combine Parquet files into a single dataset
+    print("Merge parquet file into a single dataset ...")
     table = pa.concat_tables(tables)
 
     # Convert the combined PyArrow Table to a Pandas self.df
+    print("Convert to Pandas DataFrame ...")
     self.df = table.to_pandas()
-    end_time = datetime.now()
-    duration = end_time - start_time
-    print(f'Complete extract data with duration: {duration}')
-    print('Exlpore data ...')
-    print(("========================"))
-    print(f'Total row: {self.df.shape[0]}')
-    print(f'Total column: {self.df.shape[1]}')
-    print(("========================"))
-    print(f'\n')
-    print('Show information of Dataframe')
-    print(("========================"))
-    print(self.df.info())
-    print(("========================"))
-    print(f'\n')
-    print('Show example data')
-    print(("========================"))
-    print(self.df.head(5))
-    print(("========================"))
-    print(f'\n')
 
+    print(f'Complete extract data')
 
+    # +++++++++++++++++++++++++++++++++++++++++++
+    # print('Exlpore data ...')
+    # print("========================")
+    # print(f'Total row: {self.df.shape[0]}')
+    # print(f'Total column: {self.df.shape[1]}')
+    # print("========================")
+
+    # print('Show information of Dataframe')
+    # print("========================")
+    # print(self.df.info())
+    # print("========================")
+
+    # print('Show example data')
+    # print("========================")
+    # print(self.df['create_at'].head(5))
+    # print("========================")
+    # +++++++++++++++++++++++++++++++++++++++++++
+
+  @runtime
   # Create a function for transform data.
-  def transform_data(self):
+  def transform_data(self) -> None:
 
     print("########################")
     print("#### TRANSFORM DATA ####")
     print("########################")
-    print(("========================"))
-    print(self.df.info())
-    print(("========================"))
+
     # Convert datatype to ensure that these columns are in timestamp format
-    self.df['create_at'] = self.df['create_at'].dt.strftime('%Y-%m-%d %H:%M:%S')
-    self.df['product_expire'] = self.df['product_expire'].dt.strftime('%Y-%m-%d %H:%M:%S')
+    print("Converting create_at to Timestamp ...")
+    self.df['create_at'] = pd.to_datetime(self.df['create_at'], format='%Y-%m-%d %H:%M:%S')
+
+    print("Converting product_expire to Timestamp ...")
+    self.df['product_expire'] = pd.to_datetime(self.df['product_expire'], format='%Y-%m-%d %H:%M:%S')
 
 
-  # Create a function for data ingestion.
-  def load_data(self, Truncate = True) -> None:
 
-    """
-    Ingestion data into target table.
+  @runtime
+  # Create a function for reconcile data.
+  def reconcile_data(self, tgt_table, TOTAL_REC : int) -> None:
 
-    Parameters:
-        - Truncate (boolean, optional): If True, truncate the target table before ingestion. If False, append data to the table (default=True).
-    """
+    print("########################")
+    print("#### RECONCILE DATA ####")
+    print("########################")
+
+    print("++++++++++++++++++++++++")
+    print(f"Target table : {tgt_table}")
+    print("++++++++++++++++++++++++")
 
     conn = self.create_connection_postgresql()
     cur = conn.cursor()
 
-    TOTAL_REC = len(self.df)
+    # Check record count in table
+    cur.execute(f"SELECT COUNT(1) FROM {tgt_table}")
+    result_rec_count = cur.fetchall()
+
+    if result_rec_count[0][0] == TOTAL_REC:
+      rec_count_status = "SUCCESS"
+    else:
+      rec_count_status = "FAILED"
+
+    # Check null in table
+    columns_str_chk_null = " IS NULL OR ".join(self.column_names) # Concatenate column name with IS NULL OR for create a condition check null
+    CONDITION = columns_str_chk_null+" IS NULL" # for last column
+
+    QUERY_CHK_NULL = f"""
+        SELECT COUNT(1)
+        FROM {tgt_table}
+        WHERE {CONDITION}
+    """
+
+    cur.execute(QUERY_CHK_NULL)
+    result_chk_null = cur.fetchall()
 
 
-    # Truncate the data every time, under the assumption that there is only one set of data for ingestion.
-    # If in the future there is a need to insert additional data, this section will need to be modified.
-    if Truncate:
-      try:
-        print(f"START TRUNCATE TABLE `{self.tgt_table}` ....")
-        cur.execute(f"TRUNCATE TABLE {self.tgt_table}")
-        conn.commit()
-        print(f"TRUNCATE TABLE `{self.tgt_table}` >> COMPLETE")
-      except Exception as e:
-        print(f"TRUNCATE TABLE `{self.tgt_table}` >> FAILED", e)
+    if result_chk_null[0][0] == 0:
+      chk_null_status = "SUCCESS"
+    else:
+      chk_null_status = "FAILED"
 
-    print(f"======== START INGEST DATA INTO `{self.DB}`.`{self.tgt_table}` ========")
+    print(f"======== RECONCILE PROCESS ========")
+    print(f"Source rows = {TOTAL_REC}")
+    print(f"Target rows = {result_rec_count[0][0]}")
+    print(f"Record count status = {rec_count_status}")
+    print(f"Check null status = {chk_null_status}")
+  
+    cur.close()
+    conn.close()
+
+  @runtime
+  # Create a function for data ingestion.
+  def load_data(self) -> None:
+
+    # Define the start and end dates of the range
+    start_date = datetime(2023, 1, 1)
+
+    #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+
+    print("=======================================")
+    print(f"Create csv file from filtered data ...")
+    print("=======================================")
+
+    # Check if the directory exists
+    if not os.path.exists(self.output_csv_path):
+        
+        # If it doesn't exist, create the directory
+        os.makedirs(self.output_csv_path)
+        print(f"Directory '{self.output_csv_path}' created.")
+
+    else:
+        print(f"Directory '{self.output_csv_path}' already exists.")
+
+    #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#  
+
+    for day in range(0, 8): # Specfic date with in this range
+        
+        date = start_date + timedelta(days=day)
+        
+        # Format the current date as a string in 'YYYY-MM-DD' format
+        formatted_date = date.strftime('%Y-%m-%d')
+        file_csv = f"{self.output_csv_path}/data_date_{formatted_date}.csv"
+        
+        # Filter the DataFrame for the current date and assign it to the variable
+        filtered_data = self.df[self.df['create_at'].dt.date == date.date()]
+
+        # Collect list of total record in order to reconcile data 
+        self.list_total_rec.append(filtered_data.shape[0])
+
+        # Write output filtered_data into csv file
+        filtered_data.to_csv(file_csv, index=False) 
+
+    #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#  
+
+    # Specify the date range
+    date_range = [start_date + timedelta(days=day) for day in range(0, 8)]
+
+    # Create a list to store the filtered dataframes and total record counts
+    filtered_data_list = []
+
+    for date in date_range:
+        # Filter the DataFrame for the current date
+        filtered_data = self.df[self.df['create_at'].dt.date == date.date()]
+        
+        # Collect the total record count
+        total_records = filtered_data.shape[0]
+        self.list_total_rec.append(total_records)
+        
+        # Append the filtered data and total count to the list
+        filtered_data_list.append((date, total_records, filtered_data))
+
+    # Specify the directory to save CSV files
+    output_dir = self.output_csv_path
+
+    # Batch write the data to CSV files
+    for date, total_records, filtered_data in filtered_data_list:
+        formatted_date = date.strftime('%Y-%m-%d')
+        file_csv = f"{output_dir}/data_date_{formatted_date}.csv"
+        filtered_data.to_csv(file_csv, index=False)
+
+    #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#   
+
+    for column_name in self.df.columns:
+      self.column_names.append(f'"{column_name}"')
+
+    columns_str = ", ".join(self.column_names) # List all column separated by ", "
+    
+    print("====================================")
+    print('List columns for insert into target:')
+    print("++++++++++++++++++++++++++++++++++")
+    print(f'{columns_str}')
+    print("====================================")
+    
+    print("============================================")
+    print(f"Create list of tuple contain data value ...")
+    print("============================================")
 
     try:
-      for index, row in self.df.iterrows():
-        column_names = []
-        values = []
 
-        for column_name in self.df.columns:
+      pg_hook_load = PostgresHook(postgres_conn_id=self.connection)
 
-            # Enclose column names with "" for case-sensitivity
-            column_names.append(f'"{column_name}"')
-            values.append(row[column_name])
+      for day in range(0, 8): # Specfic date with in this range
 
-        columns_str = ", ".join(column_names)
+        date = start_date + timedelta(days=day)
+        
+        # Format the current date as a string in 'YYYY-MM-DD' format
+        formatted_date_str = date.strftime('%Y-%m-%d')
 
-        # The data contains various data types, and these placeholders will be used in a join operation to create a set of columns.
-        placeholders = ", ".join(["%s"] * len(values))
-        values = tuple(values)
+        # Set the COPY command
+        copy_sql = f"""
+            COPY {self.tgt_table}_{day+1:02}({columns_str})
+            FROM stdin WITH CSV HEADER
+            DELIMITER as ','
+        """
 
-        QUERY_INSERT = f"INSERT INTO {self.tgt_table} ({columns_str}) VALUES ({placeholders})"
-        self.execute_query(conn, cur, QUERY_INSERT, values)  # Use parameterized query with values list
+        print(copy_sql)
 
-        if (index % self.BatchSize == 1+0e-2 ) or ((index + 1) % TOTAL_REC ==  0):
-          conn.commit()
+        # Get the file path as a string
+        file_path = f"{self.output_csv_path}/data_date_{formatted_date_str}.csv"
 
-          message : str = f"ROWS INSERT STATUS ------ [{index}/{TOTAL_REC}] ------"
-          print(message)
+        # Open and execute the COPY command using the pg_hook
+        print(f"======== START INGEST DATA INTO CHILD TABLE `{self.tgt_table}_{day+1:02}` ========")
 
+        with open(file_path, 'r') as file:
+            pg_hook_load.copy_expert(sql=copy_sql, filename=file.name)
 
-      # Check record count in table
-      cur.execute(f"SELECT COUNT(1) FROM {self.tgt_table}")
-      result_rec_count = cur.fetchall()
+        # self.reconcile_data(f'{self.tgt_table}_{day+1:02}', self.list_total_rec[day])
 
-      if result_rec_count[0][0] == TOTAL_REC:
-        rec_count_status = "SUCCESS"
-      else:
-        rec_count_status = "FAILED"
+        print(f"======== FINISH INGEST DATA INTO CHILD TABLE `{self.tgt_table}_{day+1:02}` ========")
 
-      # Check null in table
-      # Concatenate column name with IS NULL OR for create a condition check null
-      columns_str_chk_null = " IS NULL OR ".join(column_names)
-      CONDITION = columns_str_chk_null+" IS NULL" # for last column
-
-      QUERY_CHK_NULL = f"""
-          SELECT COUNT(*)
-          FROM {self.tgt_table}
-          WHERE {CONDITION}
-      """
-
-      cur.execute(QUERY_CHK_NULL)
-      result_chk_null = cur.fetchall()
-
-
-      if result_chk_null[0][0] == 0:
-        chk_null_status = "SUCCESS"
-      else:
-        chk_null_status = "FAILED"
-
-      print(f"======== RECONCILE PROCESS ========")
-      print(f"Source rows = {TOTAL_REC}")
-      print(f"Target rows = {result_rec_count[0][0]}")
-      print(f"Record count status = {rec_count_status}")
-      print(f"Check null status = {chk_null_status}")
+      # self.reconcile_data(f'{self.tgt_table}', sum(self.list_total_rec))
 
     except Exception as e:
         raise("Something went wrong", e)
     
-    cur.close()
-    conn.close()
-
+      
